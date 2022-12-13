d02abe9fde92c184cddf0a117dac7fedaed15e9c
plaristote <michael@unetresgrossebite.com>
2022-12-13 14:16:29 +0100

    rewrite storage method

--- libcrails-attachment/crails/attachment.cpp
+++ libcrails-attachment/crails/attachment.cpp
@@ -1,7 +1,10 @@
 #include "attachment.hpp"
 #include <crails/utils/random_string.hpp>
 #include <crails/utils/split.hpp>
+#include <crails/utils/join.hpp>
 #include <filesystem>
+#include <crails/logger.hpp>
+#include <crails/server.hpp>
 
 using namespace std;
 using namespace Crails;
@@ -10,13 +13,18 @@ Attachment::Attachment(const string& uid) : std::string(uid)
 {
   list<string> parts = split(uid, '.');
 
-  if (parts.size() > 3)
+  if (parts.size() >= 3)
   {
     auto part = parts.rbegin();
 
     extension = *part++;
     mimetype  = *part;
+    for (int i = 0 ; i < 2 ; ++i)
+      parts.erase(std::next(parts.rbegin()).base());
+    name = join(parts, '.');
   }
+  else
+    name = uid;
 }
 
 static string find_extension_in_filename(const string& value)
@@ -32,10 +40,13 @@ void Attachment::use_uploaded_file(const Crails::Params::File* file)
 {
   if (file)
   {
-    cleanup_files();
+    if (length() > 0)
+      cleanup_files();
     extension = find_extension_in_filename(file->name);
     mimetype = file->mimetype;
     generate_uid();
+    logger << Logger::Info << "storing attachment at " << get_filepath() << Logger::endl;
+    filesystem::create_directories(get_store_path());
     filesystem::copy_file(file->temporary_path, get_filepath());
   }
   else
@@ -44,7 +55,10 @@ void Attachment::use_uploaded_file(const Crails::Params::File* file)
 
 void Attachment::cleanup_files()
 {
-  filesystem::remove(get_filepath());
+  auto path = get_filepath();
+
+  if (filesystem::exists(path))
+    filesystem::remove(path);
 }
 
 void Attachment::generate_uid()
@@ -52,24 +66,53 @@ void Attachment::generate_uid()
   std::string       charset("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890");
   std::stringstream stream;
 
-  stream << std::time(0) << '_';
-  stream << generate_random_string(charset, 5);
+  if (name.length())
+    stream << name;
+  else
+  {
+    stream << std::time(0) << '_';
+    stream << generate_random_string(charset, 5);
+  }
+  name = stream.str();
   std::string::operator=(stream.str() + '.' + mimetype + '.' + extension);
 }
 
-std::string Attachment::get_path() const
+std::string Attachment::get_store_path() const
 {
-  if (length() == 0)
-    return get_default_store_path();
-  return '/' + get_default_store_path() + '/' + get_filename();
+  auto public_paths = Server::singleton::require().get_public_paths();
+
+  if (!public_paths.size())
+    throw boost_ext::runtime_error("No public paths found for storing attachments.");
+  return *public_paths.begin() + "/attachments";
+}
+
+std::string Attachment::get_url() const
+{
+  auto public_paths = Server::singleton::require().get_public_paths();
+  std::string store_path = get_store_path();
+  std::string url;
+
+  for (auto public_path : public_paths)
+  {
+    if (store_path.find(public_path) == 0)
+    {
+      url = get_filepath().substr(public_path.length());
+      break ;
+    }
+  }
+  if (!url.length())
+    throw boost_ext::runtime_error("Attachment does not have any URL.");
+  if (url[0] == '/')
+    return url;
+  return '/' + url;
 }
 
 std::string Attachment::get_filepath() const
 {
-  return filesystem::current_path().string() + '/' + get_default_store_path() + '/' + get_filename();
+  return get_store_path() + '/' + get_filename();
 }
 
 std::string Attachment::get_filename() const
 {
-  return (*this) + '.' + extension;
+  return name + '.' + extension;
 }
--- libcrails-attachment/crails/attachment.hpp
+++ libcrails-attachment/crails/attachment.hpp
@@ -14,26 +14,22 @@ namespace Crails
 
     Attachment(const std::string& uid);
 
+    const std::string& get_name() const { return name; }
     const std::string& get_extension() const { return extension; }
     const std::string& get_mimetype() const { return mimetype; }
-    std::string get_path() const;
 
     void use_uploaded_file(const Crails::Params::File* file);
 
     virtual void cleanup_files();
 
-    virtual std::string get_default_store_path() const
-    {
-      return Attachment::default_store_path;
-    }
+    virtual std::string get_store_path() const;
+    virtual std::string get_url() const;
 
   protected:
     std::string get_filepath() const;
     std::string get_filename() const;
     void        generate_uid();
-
-    const static std::string default_store_path;
-    std::string              extension, mimetype;
+    std::string name, extension, mimetype;
   };
 }
 
